a53fdc1b3d6a6d3bb9760c54470a7d19c2d711a7
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-04-21 11:49:51 +0000

    third commit

--- src/components/About.js
+++ src/components/About.js
@@ -11,16 +11,28 @@ const About = () => {
 
   return <section id='about' className='section' ref={ref}>
            <div className="container mx-auto">
+            <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
             {/*img */}
-            <div className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
-            </div>
+            <motion.div 
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once:false,amount:0.3 }}
+
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+            </motion.div>
             {/*text */}
-            <div>
-             <h2>About Me</h2>
-             <h3>  I'm a Freelance Front-end Developer 
+            <motion.div 
+              variants={fadeIn('left', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{once:false,amount:0.3 }}
+              className='flex-1 '>
+             <h2 className='h2 text-accent'>About Me</h2>
+             <h3 className='h3 mb-4'>  I'm a Freelance Front-end Developer 
                    with over 2 years of experience
             </h3>
-             <p>
+             <p className='mb-6'>
              I am an experienced and dedicated web developer, I bring a strong set of 
              skills and a passion for solving problems to every project. With a major
             focus on JavaScript React, I am proficient in technologies
@@ -29,17 +41,42 @@ const About = () => {
             and testable, but also dynamic.
              </p>
              {/*stats*/}
-             <div>
+             <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
-                 { inView? <Countup start={0} end={2} duration={2} /> : 2 }
-                </div>
+                 { inView? <Countup start={0} end={2} duration={4} /> : 2 }
+                   </div>
                   <div className='font-primary text-sm tracking-[2px]'>
                     Years of <br/>
                     Experience
                   </div>
               </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                 { inView? <Countup start={0} end={20} duration={8} /> : 20 }+
+                   </div>
+                  <div className='font-primary text-sm tracking-[2px]'>
+                    Projects <br/>
+                    Completed
+                  </div>
+              </div>
+
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                 { inView? <Countup start={0} end={10} duration={8} /> : 10 }+
+                   </div>
+                  <div className='font-primary text-sm tracking-[2px]'>
+                    Satisfied <br/>
+                    Clients
+                  </div>
+              </div>
              </div>
+             <div className='flex gap-x-8 items-center'>
+                  <button className='btn btn-lg'>Contact Me</button>
+                  <a className='text-gradient btn-link' href="#">My Portfolio</a>
+                </div>
+            </motion.div>
             </div>
            </div>
         </section>;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import {BsArrowUpRight} from 'react-icons/bs'
+import {motion} from 'framer-motion'
+import { fadeIn } from '../variants';
 
 const Services = () => {
-  return <div id='services' className='section'>
-            Services
-          </div>;
+const services =[
+  {
+    name:'UI/UX Design',
+    description:'I can collabrate with UI/UX designers to raise user satisfaction and ensure user-based company goals are reached. Such goals might include increasing newsletter sign-ups and also improving sales conversions',
+    link:'Learn More',
+  },
+  {
+    name:'Digital Marketing ',
+    description:'Digital Marketting requires an advance level of problem-solving and analytical thinking. I have the ability to analyze and market to the public and visualize several possible ways to advertise to the world with the help of marketting tools like PowerBi and Google trends to acertain the best solution.',
+    link:'Learn More',
+  },
+  {
+    name:'Producting Branding',
+    description:'I rise to the challenge of updating and keeping in touch with the latest in technologies to brand my products. I have enormous interest in product branding and I beleive it plays the vital role marketing.',
+    link:'Learn More',
+  },
+]
+
+  return <section id='services' className='section'>
+            <div className="container mx-auto">
+              <div>
+                {/*texts & image*/}
+                <div className='flex-1 lg:bg-services lg:bg-bottom bg-no-repeat mix-blend-lighten mb-12'>
+                  <h2 className='h2 text-accent mb-6'>What I Do</h2>
+                  <h3 className='h3 max-w-[455px] mb-16 '>I'm a Freelance Front-end Developer 
+                   with over 2 years of experience</h3>
+                   <button className='btn btn-sm'>See My Work</button>
+                </div>
+                {/*services*/}
+                <div>Services</div>
+              </div>
+            </div>
+          </section>;
 };
 
 export default Services;
